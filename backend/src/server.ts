8a47233bbdc26c77624aefc1e84087c955c7d68b
brunohhomem <brunohhomem@gmail.com>
2025-05-21 12:18:21 -0300

    refactor(server): reorganize server with `app.ts`

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,5 @@
-import fastify, { FastifyReply, FastifyRequest } from "fastify";
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import prismaClient from "./prisma";
-import { routes } from "./routes";
+import app from "./app"
 
-const app = fastify({ logger: true })
-
-const start = async () => {
-
-  app.register(routes)
-
-  app.listen({ port: 8000 }, () => {
-    console.log("server running...🚀")
-  })
-}
-
-start()
+app.listen({ port: 8000 }, () => {
+  console.log("server running...🚀")
+})
