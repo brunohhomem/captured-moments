1a3e150f3caaa999520c806b2331f0d5ce0d6e2a
brunohhomem <brunohhomem@gmail.com>
2025-05-24 11:08:45 -0300

    feat(frontend): add login, home and signUp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { Login } from "./pages/Auth/Login";
+import { SignUp } from "./pages/Auth/SignUp";
+
 export function App() {
   return (
-    <h1>codeweek</h1>
+    <div>
+      <h1>codeweek</h1>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+
+      </Router>
+    </div>
   )
 }
